eff8cd14a6648a97b0e139fdf0a88b412cc6dec2
Helene Høie <helenehoie@gmail.com>
2020-02-03 09:13:44 +0100

    update

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -6,7 +6,7 @@ const Cv = () => {
         <main className='about'>
             <div className='profile-text'>
                 <h1><span>ui/ux</span>designer</h1>
-                <p>text.........</p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 <p><a href='mailto:helenehoie@gmail.com'>helenehoie@gmail.com</a>+47 928 83 244</p>
                 <a href="https://www.linkedin.com/in/helenehoienaesheim/" target="_blank">linkedin</a>
             </div>
