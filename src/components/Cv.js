a6e394fe05e788c5373508a497d97ee7cc38d363
Helene Høie <helenehoie@gmail.com>
2020-06-02 12:19:22 +0200

    update tekst cv

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -6,7 +6,7 @@ const Cv = () => {
         <main className='about'>
             <div className='profile-text'>
                 <h1><span>UI/UX</span>designer</h1>
-                <p>UI/UX designer som elsker å utforme det visuelle grensesnittet og den logiske tenkningen rundt front-end. Jeg elsker å jobbe med hele designprosessen fra start til slutt. </p>
+                <p>UI/UX designer som elsker å utforme det visuelle grensesnittet. Jeg elsker å jobbe med hele designprosessen fra start til slutt. </p>
 
                 <p className='space-between'>Du kan kontakte meg på <a href='mailto:helenehoie@gmail.com'>helenehoie@gmail.com</a> eller så finner du meg også på <a href="https://www.linkedin.com/in/helenehoienaesheim/" target="_blank">linkedin</a></p>
             </div>
